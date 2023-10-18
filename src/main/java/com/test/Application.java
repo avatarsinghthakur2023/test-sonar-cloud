a2501876c7ddd438420cd6c558b469a5c2af07c8
Hasnain <hasnain.alibhai@strawberry.com>
2023-10-18 11:51:05 +0200

    Adding twitter dependencies

--- src/main/java/com/test/Application.java
+++ src/main/java/com/test/Application.java
@@ -1,9 +1,20 @@
 package com.test;
 
+import twitter4j.Twitter;
+import twitter4j.v1.Status;
+
+import java.util.List;
+
 public class Application {
-    public static void main (String[] args){
+    public static void main (String[] args) throws Exception{
         System.out.println("Hello world");
         System.out.println("Avatar ");
-
+        Twitter twitter = Twitter.getInstance();
+        List<Status> statuses = twitter.v1().timelines().getHomeTimeline();
+        System.out.println("Showing home timeline.");
+        for (Status status : statuses) {
+            System.out.println(status.getUser().getName() + ":" +
+                    status.getText());
+        }
     }
 }
