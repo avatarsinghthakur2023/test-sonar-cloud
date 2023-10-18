aa1b8fcfc4a2a4c986e7234065a9f625a3bcf9cf
Hasnain <hasnain.alibhai@strawberry.com>
2023-10-18 11:23:19 +0200

    First commit

--- .idea/src/main/java/com/test/Application.java
+++ .idea/src/main/java/com/test/Application.java
@@ -3,5 +3,7 @@ package com.test;
 public class Application {
     public static void main (String[] args){
         System.out.println("Hello world");
+        System.out.println("Avatar ");
+
     }
 }
